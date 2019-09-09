94e2be4056ac102d9af1e123aa80d6f30d5ad1fb
Lozo <45313263+Azizcheb@users.noreply.github.com>
2019-09-09 13:30:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,38 +1,27 @@
-const prefix = ">";
+
 const Discord = require("discord.js");
-const client = new Discord.Client;
 
- let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  
-  
-  if(cmd === `${prefix}ping`) {
-  Client.commands.get('ping').execute(message, args);
+
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
   }
-
-if(cmd === `${prefix}serverinfo`){
-
-    //let sicon = guild.displayAvatarURL;
-    let serverembed = new Discord.RichEmbed()
-
-    .setDescription("Server Information")
-    .setColor("#15f153")
-    //.setThumbnail(sicon)
-    .addField("Server Name", message.guild.name)
-    .addField("Created On", message.guild.createdAt)
-    .addField(`Owner`, `${message.guild.owner.user.tag}`, true)
-    .addField(`Members`, `${message.guild.members.size} (${message.guild.presences.filter(p => p.status === 'online').size} online, ${message.guild.presences.filter(p => p.status === 'idle').size} idle, ${message.guild.presences.filter(p => p.status === 'dnd').size} dnd, ${message.guild.presences.filter(p => p.status === 'dnd').size} invisible/offline)`, true)
-    .addField("You Joined", message.member.joinedAt)
-    //.addField("Total Members", message.guild.memberCount);
-    .addField(`Features`, `${message.guild.features.join(', ') || 'None'}`, true)
-    .addField(`Roles`, `${message.guild.roles.filter(r => r.position !== 0).map(R => R.name).join(', ')}`, true) // Filter is to filter out @everyone role
-
-
-    return message.channel.send(serverembed);
-  }
-
-
+});
 
   
 
